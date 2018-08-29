e5b9d9192d5bcf937b7c2933d9af1d73bc7a912b
sigmasigma <kotagushima@gmail.com>
2018-08-29 11:15:46 +0900

    自機移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+  	// 移動スピード
+  	public float speed = 5;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,16 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// 右・左
+    	float x = Input.GetAxisRaw ("Horizontal");
+    	// 移動する向きを求める
+    	Vector2 direction = new Vector2 (x, 0).normalized;
+    	// 移動する向きとスピードを代入する
+    	GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+		if (Input.GetKey(KeyCode.Space)){
+			// スペースで射撃
+			
+		}
 	}
 }
